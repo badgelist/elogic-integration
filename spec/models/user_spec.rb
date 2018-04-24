589bed1f38227c8ea74fa3942c25960b2b4de057
Hank Holiday <ryan.hank@gmail.com>
2018-04-24 11:02:11 -0700

    Added basic user model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "has a valid factory" do
-    expect(user).to be_valid
+
+  it 'has a valid factory' do
+    expect(build(:user)).to be_valid
   end
+
+  let(:user) { build(:factory_you_built) }
+
+  describe 'ActiveRecord fields and associations' do
+
+    it { expect(user).to have_attributes :name, :email, :password }
+
+  end
+
 end
